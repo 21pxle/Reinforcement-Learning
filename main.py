88e9973d06e2585b9710a5d47fc41f47491046a9
Patrick <43623131+21pxle@users.noreply.github.com>
2024-10-15 10:41:30 -0400

    Updated condition of termination to successful completion.

--- main.py
+++ main.py
@@ -6,16 +6,15 @@ import numpy as np
 from dqn import Agent
 from utils import plot_learning_curve
 
-random.seed(0)
-
 if __name__ == '__main__':
     env = gym.make("CartPole-v1", render_mode="rgb_array")
     agent = Agent(gamma=0.99, epsilon=1.0, batch_size=64, n_actions=2,
                   eps_min=0.01, input_dims=[4], lr=0.001)
     scores, eps_history = [], []
-    n_games = 200
+    n_games = 0
+    has_truncated = False
 
-    for i in range(n_games):
+    while not has_truncated:
         score = 0
         done = False
         observation, _ = env.reset()
@@ -24,14 +23,16 @@ if __name__ == '__main__':
             new_observation, reward, terminated, truncated, info = env.step(action)
             score += reward
             done = terminated or truncated
+            has_truncated = truncated
             agent.store_transition(observation, action, reward, new_observation, done)
             agent.learn()
             observation = new_observation
         scores.append(score)
         eps_history.append(agent.epsilon)
-        avg_score = np.mean(scores[-100:])
+        avg_score = np.mean(scores[-20:])
+        n_games += 1
 
-        print(f'Episode {i} Results')
+        print(f'Episode {n_games + 1} Results')
         print(f'Score: {score:.2f}')
         print(f'Average Score: {avg_score:.2f}')
         print(f'Epsilon: {agent.epsilon:.2f}')
